ef9498fb094c4af55c709a52c698dfaed0cf2a26
Tyler Furby <tyler@tylerfurby.com>
2017-07-10 19:21:10 -0600

    Added welcome component

---
+++ src/app/welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'welcome-detail',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+
+export class WelcomeComponent {
+
+}
